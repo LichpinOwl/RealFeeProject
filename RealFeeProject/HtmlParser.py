7790862bc2f096f27244e29aa1754e71fc506d28
Xaos <32937400+XaosFortuna@users.noreply.github.com>
2019-02-19 15:45:42 +0330

    Add files via upload

--- RealFeeProject/HtmlParser.py
+++ RealFeeProject/HtmlParser.py
@@ -10,10 +10,18 @@ import os
 cwd = os.path.dirname(os.path.realpath(__file__)) + '\\'
 
 
-def CurrencyPriceListCreator(inputHtml, delimiter):
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
+def CurrencyPriceListCreator(inputHtml, delimiter1, delimiter2):
 	try:
 		DateTimeList = []
 		PriceList = []
+		TempList = []
 		cwd = os.path.dirname(os.path.realpath(__file__)) + '\\'
 
 		with open(inputHtml, 'r') as HtmlFile:
@@ -21,58 +29,56 @@ def CurrencyPriceListCreator(inputHtml, delimiter):
 		# Parse the html file
 		soup = BeautifulSoup(html_doc, 'html.parser')
 		with open(cwd + 'Output.html', 'w') as o:
-			for i in  soup.findAll("div", {"class": "text"}):
-				if delimiter in str(i) and '\xD8\xB3\xD8\xA8\xD8\xB2\xD9\x87\x20\xD9\x85\xDB\x8C\xD8\xAF\xD9\x88\xD9\x86'\
-				not in str(i) and '\xD8\xB3\xDA\xA9\xD9\x87' not in str(i): #and '\xD9\x82\xDB\x8C\xD9\x85\xD8\xAA\x20\xD9\x84\xD8\xAD\xD8\xB8\xD9\x87\x20\xD8\xA7\xDB\x8C\x20\x3C' in str(i):
-					o.write(str(i) + '\n')
-
+			for t in  soup.findAll("div", {"class": "text"}):
+				TempList.append(str(t))
+			for item in TempList:
+				if '\xD9\x82\xDB\x8C\xD9\x85\xD8\xAA\x20\xD9\x84\xD8\xAD\xD8\xB8\xD9\x87\x20\xD8\xA7\xDB\x8C\x20\xD8\xAF\xD9\x84\xD8\xA7\xD8\xB1' in item and delimiter1 in item and delimiter2 in item :
+					o.write(str(item)[19:int(find_nth(item, '<br/><br/>', 2)) - 12] + '\n')
+		
 		with open(cwd + 'Output.html', 'r') as f:
 			for i in f.readlines():
-				if "<div" not in i :
-					if "</div" not in i:
-						try:
-						# remove tags : re.sub(r'<.+?>', '', i)
-							DateTimeList.append((datetime.datetime.strptime(i[36:46] + '-' + i[51:59], "%Y-%m-%d-%H:%M:%S")))
-							PriceList.append(i[214:219])
-						except:
-							DateTimeList.append((datetime.datetime.strptime(i[97:107] + '-' + i[112:120], "%Y-%m-%d-%H:%M:%S")))
-							PriceList.append(i[274:280])
+				# print i.rfind('>')
+				try:
+					# remove tags : re.sub(r'<.+?>', '', i)
+					# if (i[36:46]).isdigit():
+					print datetime.datetime.strptime(i[36:46] + '-' + i[51:59], "%Y-%m-%d-%H:%M:%S")
+					DateTimeList.append((datetime.datetime.strptime(i[36:46] + '-' + i[51:59], "%Y-%m-%d-%H:%M:%S")))
+					print len(DateTimeList)
+					print i[int(i.rfind('>')) + 2:]
+					PriceList.append(i[int(i.rfind('>')) + 2:])
+					print len(PriceList)
+				except ValueError:
+					continue
+				except Exception as e:
+					print str(e)
 		return (DateTimeList, PriceList)
-
-
 
 	except Exception as e:
 		print str(e)
 
 
-
-
-
-USDKaghazi = '\xDA\xA9\xD8\xA7\xD8\xBA\xD8\xB0\xDB\x8C'
 x = []
 y = []
 
+HaratDollar = '\xD9\x87\xD8\xB1\xD8\xA7\xD8\xAA'
+NaghdiDollar = '\xD9\x86\xD9\x82\xD8\xAF\xDB\x8C'
+SoleimaniehDollar = '\xD8\xB3\xD9\x84\xDB\x8C\xD9\x85\xD8\xA7\xD9\x86\xDB\x8C\xD9\x87'
+KaghaziDollar = '\xDA\xA9\xD8\xA7\xD8\xBA\xD8\xB0\xDB\x8C'
 
 
-
-for FileName in range(29,47) :
+for FileName in range(10, 40) :
 	print FileName
-	a, b = CurrencyPriceListCreator(cwd + 'messages' + str(FileName) + '.html',USDKaghazi)
+	a, b = CurrencyPriceListCreator(cwd + 'messages' + str(FileName) + '.html', NaghdiDollar, HaratDollar)
 	for i in a:
 		x.append(i)
 	for j in b:
 		y.append(j)
 	print 'len x : ' +  str(len(x))
 	print 'len y : ' +  str(len(y))
+	print '*****************************************'
 
 
 
-
-# for i in x :
-# 	 print i
-
-# for i in y :
-# 	 print i
 
 fig = go.Figure([go.Scatter(x = x, y = y)])
 py.offline.plot(fig)
