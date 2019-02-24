a690edfc45a9b1f38aa7989433fcb56c4eab1464
Xaos <32937400+XaosFortuna@users.noreply.github.com>
2019-02-24 15:44:47 +0330

    Add files via upload

--- RealFeeProject/HtmlParser.py
+++ RealFeeProject/HtmlParser.py
@@ -8,48 +8,43 @@ import plotly.graph_objs as go
 import os
 import sqlite3
 import Config
+import ntpath
 
 
-
-
-def DBConfig(DBName, TableName):
+def CreateTable(DBName, TableName):
     try:
-        con = sqlite3.connect(Config.cwd + Config.DBName)
-        print 'creating database'.title()
-        con.execute('CREATE TABLE ' + Config.TableName + \
-                    	   ''' (ID 			INTEGER PRIMARY KEY AUTOINCREMENT,
-				DateTime 		TEXT,
-			        HaratDollar       	INT ,
-			        IstanbulDollar		INT ,
-			        SoleimaniehDollar	INT ,
-			        KaghaziDollar		INT 
-			        );'''
-                    )
+        con = sqlite3.connect(Config.cwd + DBName)
+        print 'creating Table: Table'.title() + str(TableName) + '\n'
+        con.execute('CREATE TABLE Table' + TableName + '(ID INTEGER PRIMARY KEY AUTOINCREMENT, DateTime TEXT, ' + TableName + ' INT)')
         con.close()
     except Exception as e:
         print 'error in create db or table : '.title() + str(e)
 
-def InsertTODB(DBName, TableName, ColumnsList, ValuesList):
+
+def InsertToTable(DBName, TableName, ColumnsList, ValuesList):
     try:
-    	print 'inserting data to database'.title()
-        con = sqlite3.connect(Config.cwd + Config.DBName)
+        con = sqlite3.connect(Config.cwd + DBName)
         con.execute(
-            'INSERT INTO ' + Config.TableName + ' (' + (', ').join(ColumnsList) + ') VALUES (\'' + ('\', \'').join(ValuesList) + '\')')
+            'INSERT INTO ' + TableName + ' (' + (', ').join(ColumnsList) + ') VALUES (\'' + ('\', \'').join(
+                ValuesList) + '\')')
         con.commit()
         con.close()
     except Exception as e:
         print 'error in inserting to table : '.title() + str(e)
 
+
 def UpdateTable(DBName, TableName, ColumnName, ColumnValue, ID):
     try:
-    	print 'Updating table'.title()
-        con = sqlite3.connect(Config.cwd + Config.DBName)
+        print 'Updating table'.title()
+        con = sqlite3.connect(Config.cwd + DBName)
+        print 'UPDATE ' + TableName + ' SET ' + ColumnName + '= ' + ColumnValue + ' WHERE ID = ' + ID
         con.execute(
-            'UPDATE ' + Config.TableName + ' SET ' + ColumnName + '= ' + ColumnValue + ' WHERE ID = ' + ID)
+            'UPDATE ' + TableName + ' SET ' + ColumnName + '= ' + ColumnValue + ' WHERE ID = ' + ID)
         con.commit()
         con.close()
     except Exception as e:
-        print 'error in inserting to table : '.title() + str(e)	
+        print 'error in inserting to table : '.title() + str(e)
+
 
 def find_nth(haystack, needle, n):
     start = haystack.find(needle)
@@ -58,7 +53,8 @@ def find_nth(haystack, needle, n):
         n -= 1
     return start
 
-def CurrencyPriceListCreator(inputHtml, delimiter1, delimiter2):
+
+def CurrencyPriceListCreator(inputHtml, delimiter1, delimiter2, OutputFile):
     try:
         DateTimeList = []
         PriceList = []
@@ -69,15 +65,15 @@ def CurrencyPriceListCreator(inputHtml, delimiter1, delimiter2):
             html_doc = HtmlFile.read()
         # Parse the html file
         soup = BeautifulSoup(html_doc, 'html.parser')
-        with open(Config.cwd + 'Output.html', 'w') as o:
+        with open(OutputFile, 'w') as o:
             for t in soup.findAll("div", {"class": "text"}):
                 TempList.append(str(t))
             for item in TempList:
-                if '\xD9\x82\xDB\x8C\xD9\x85\xD8\xAA\x20\xD9\x84\xD8\xAD'+\
-                '\xD8\xB8\xD9\x87\x20\xD8\xA7\xDB\x8C\x20\xD8\xAF\xD9\x84\xD8\xA7\xD8\xB1' \
-                in item and delimiter1 in item and delimiter2 in item:
+                if '\xD9\x82\xDB\x8C\xD9\x85\xD8\xAA\x20\xD9\x84\xD8\xAD' + \
+                        '\xD8\xB8\xD9\x87\x20\xD8\xA7\xDB\x8C\x20\xD8\xAF\xD9\x84\xD8\xA7\xD8\xB1' \
+                        in item and delimiter1 in item and delimiter2 in item:
                     o.write(str(item)[19:int(find_nth(item, '<br/><br/>', 2)) - 12] + '\n')
-        with open(Config.cwd + 'Output.html', 'r') as f:
+        with open(OutputFile, 'r') as f:
             for i in f.readlines():
                 try:
                     # remove tags : re.sub(r'<.+?>', '', i)
@@ -85,7 +81,7 @@ def CurrencyPriceListCreator(inputHtml, delimiter1, delimiter2):
                     DateTimeList.append(datetime.datetime.strptime(i[36:46] + '-' + i[51:59], "%Y-%m-%d-%H:%M:%S"))
                     # print len(DateTimeList)
                     # print i[int(i.rfind('>')) + 2:]
-                    PriceList.append(i[int(i.rfind('>')) + 2:])
+                    PriceList.append(i[int(i.rfind('>')) + 2:-1])
                     # print len(PriceList)
                 except ValueError:
                     continue
@@ -97,56 +93,46 @@ def CurrencyPriceListCreator(inputHtml, delimiter1, delimiter2):
         print str(e)
 
 
-
-
-
-
-
 if __name__ == "__main__":
-	os.system('cls')
+    os.system('cls')
 
-	if not os.path.isfile(Config.cwd + Config.DBName):
-		DBConfig(Config.DBName, Config.TableName)
-	else:
-		pass
+    if not os.path.isfile(Config.cwd + Config.DBName):
+        for n in Config.DelimitersList.keys():
+            CreateTable(Config.DBName, n)
+    else:
+        pass
 
-	#DATETIME LIST
-	x = []
-
-	#CURRENCYVALUESLIST
-	y = []
-	AllDataDict = {}
-	for FileName in range(10, 11):
-		print FileName
-	        for UnicodeKey, UnicodeValue in Config.DelimitersList.iteritems():
-	        	print 'process ' + UnicodeKey + ' : '
-	        	if not UnicodeValue == Config.DelimitersList['NaghdiDollar'] :
-		    		TimeList, ValuesList = CurrencyPriceListCreator(Config.cwd + 'messages' + str(FileName) + '.html', Config.DelimitersList['NaghdiDollar'], UnicodeValue)
-			   	for i in TimeList:
-			        	x.append(i)
-			    	for j in ValuesList:
-			        	y.append(j)
-			        AllDataDict[UnicodeKey] = y
-				for time, value in zip(x, y):
-					for c in range(0, len(y) + 1) :
-						UpdateTable(Config.DBName, Config.TableName, UnicodeKey, str(y[c]), str(c))
-
-	# print 'len x : ' + str(len(x))
- #        print 'len y : ' + str(len(y))
- #        print '*****************************************'
-
-	# fig = go.Figure([go.Scatter(x=x, y=y)])
-	# py.offline.plot(fig)
-	print '*'*100
-	conn = sqlite3.connect(Config.cwd + 'CurrencyDB.db')
-	cursor = conn.execute("SELECT ID, DateTime, HaratDollar, IstanbulDollar, SoleimaniehDollar, KaghaziDollar from " + Config.TableName)
-	for row in cursor:
-	   print "ID = ", row[0]
-	   print "DateTime = ", row[1]
-	   print "USD_Harat = ", row[2]
-	   print "USD_Dubai = ", row[3],
-	   print 'LIR = ', row[4], 
-	   print 'EURO = ', row[5],"\n"
-
-	print "Operation done successfully";
-	conn.close()
+    # DATETIME LIST
+    TimeList = []
+    # CURRENCY VALUES LIST
+    ValuesList = []
+    AllDataDict = {}
+    for FileName in range(10, 40):
+        print '*' * (14 + len(str(FileName)))
+        print 'File Number : ' + str(FileName)
+        print '*' * (14 + len(str(FileName)))
+        for UnicodeKey, UnicodeValue in Config.DelimitersList.iteritems():
+            print 'processing : ' + UnicodeKey
+            if UnicodeValue != Config.DelimitersList['NaghdiDollar']:
+                OutputFile = Config.cwd + 'OutputHtmls\\' + str(FileName) + '-' + UnicodeKey + '.html'
+                print OutputFile
+                TimeList, ValuesList = CurrencyPriceListCreator(Config.cwd + 'messages' + str(FileName) + '.html',
+                                                                Config.DelimitersList['NaghdiDollar'], UnicodeValue, OutputFile)
+                print 'len timeList : ' + str(len(TimeList))
+                print 'len valueList : ' + str(len(ValuesList))
+            for time, value in zip(TimeList, ValuesList):
+                InsertToTable(Config.DBName, 'Table' + UnicodeKey, ['DateTime', UnicodeKey], [str(time), value])
+    # fig = go.Figure([go.Scatter(x=x, y=y)])
+    # py.offline.plot(fig)
+            conn = sqlite3.connect(Config.cwd + Config.DBName)
+            # print "SELECT ID, DateTime, " + UnicodeKey +  " from " + 'Table' + UnicodeKey
+            cursor = conn.execute("select count(*) from Table" + str(UnicodeKey))
+            for row in cursor:
+                print str(UnicodeKey) + " rows count :" + str(row[0])    + '\n'
+            # for row in cursor:
+            #     print "ID = ", row[0]
+            #     print "DateTime = ", row[1]
+            #     print UnicodeKey + ' = ', row[2]
+            # print "Operation done successfully"
+            conn.close()
+        print '-'*50
