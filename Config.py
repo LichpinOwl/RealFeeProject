a06bf345db49560b95fc7d0eee141726f673586c
Xaos <32937400+XaosFortuna@users.noreply.github.com>
2019-03-04 15:30:09 +0330

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import os
+
+# PATHES
+cwd = os.path.dirname(os.path.realpath(__file__)) + '\\'
+
+# DATABASE CONFIGURATIONS
+DBName = 'CurrencyDB.db'
+TableName = 'Currencies'
+TagDict = {'Dollar': ['#دلار', '#نقدی'],  'Euro':['#یورو', 'قیمت'], 'Lir': ['#حواله_لیر', 'قیمت'], 'Tala':['#طلا', '#مظنه'], 'Yuan': ['#حواله_یوان', 'قیمت']}
+
